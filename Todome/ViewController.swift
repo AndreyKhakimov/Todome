7bb96c8e6cd16ba476aa8d040273b10e3752bf76
Andrey Khakimov <khakimov013@gmail.com>
2022-03-30 21:00:53 +0200

    Initial Commit

---
+++ Todome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todome
+//
+//  Created by Andrey Khakimov on 30.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
